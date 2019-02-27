ae3337edbbfbab4924de4e3b1ad36224ce277482
lucaluna0719 <47353566+lucaluna0719@users.noreply.github.com>
2019-02-27 12:02:30 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,52 @@
 function mostrar()
 {
+	var contador=0;
+	var acumulador=0;
+	var promedio;
+	var nota;
+	var numero;
+	var sexoNotaMasBaja;
+	var cantidadVaronesMayorASeis=0;
+	var notaMinima;
+
+	while(contador<5)
+	{
+		
+		nota=prompt("Ingrese nota:"),
+		nota= parseInt(nota);
+		while (isNaN(nota) == true || nota<0 || nota>=10 )
+		{
+			nota=prompt("error,Ingrese nota:");
+			nota= parseInt(nota);
+		}
+		sexo=prompt("ingrese sexo");
+		while (isNaN(sexo) == false || sexo != "f" && sexo!= "m" )
+		{
+			sexo=prompt("error ,ingrese sexo");
+		}
+
+		acumulador=acumulador+nota;
+		if(contador==0)
+		{
+			notaMinima=nota;
+			sexoNotaMasBaja=sexo;
+		}
+		else
+		{
+			if(notaMinima>nota)
+			{
+				notaMinima=nota;
+				sexoNotaMasBaja=sexo;
+			}
+		}
+
+		if(nota> 5 && sexo=="m")
+		{			
+				cantidadVaronesMayorASeis=cantidadVaronesMayorASeis+1;
+		}
+
+		contador=contador+1;
+	}	
+	alert(sexoNotaMasBaja);
 
 }
